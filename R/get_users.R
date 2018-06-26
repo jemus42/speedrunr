27628621281c7770d43929b1c2724e87032e248e
jemus42 <jemus42@quantenbrot.de>
2018-06-26 23:22:56 +0200

    Add get_user

---
+++ R/get_users.R
@@ -0,0 +1,34 @@
+#' Get a Single User by ID
+#'
+#' @param id The user id, e.g. `"e8e5v680"` for *zfg*.
+#' @param ... Optional parameters for the api.
+#'
+#' @return A [tibble::tibble] with one row.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_user(id = "e8e5v680")
+#' }
+get_user <- function(id, ...) {
+
+  path <- paste("users", id, sep = "/")
+
+  res <- sr_get(path, ...)
+  data <- res$data
+
+  tibble::tibble(
+    id = data$id,
+    name_int = data$names$international,
+    weblink = data$weblink,
+    role = data$role,
+    signup = lubridate::ymd_hms(data$signup),
+    location_code = pluck(data, "location", "country", "code", .default = NA),
+    location_name = pluck(data, "location", "country", "names", "international", .default = NA),
+    twitch = purrr::pluck(data, "twitch", "uri", .default = NA),
+    hitbox = purrr::pluck(data, "hitbox", "uri", .default = NA),
+    youtube = purrr::pluck(data, "youtube", "uri", .default = NA),
+    twitter = purrr::pluck(data, "twitter", "uri", .default = NA),
+    speedrunslive = purrr::pluck(data, "speedrunslive", "uri", .default = NA)
+  )
+}
