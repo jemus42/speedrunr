c8f26f19644a27f06f16eeebb56b577799f51734
Lukas Burk <lukas@quantenbrot.de>
2018-07-19 21:54:54 +0200

    Fix pagination in get_runs (hopefully for good)

--- R/get_runs.R
+++ R/get_runs.R
@@ -4,6 +4,9 @@
 #' @param category The category's id.
 #' @param max The number of runs to return, default is `100`.
 #' @param status Filter by run status, default is `"verified"`. Leave blank for all runs.
+#' @param offset Offset used for pagination, supply `200` to get the *next* 200 runs.
+#' @param verbose If `TRUE`, will display debugging output for pagination handling.
+#'   This option will probably not stick around for long.
 #' @param ... Optional arguments passed to API.
 #'
 #' @return A [tibble::tibble] of runs.
@@ -14,16 +17,18 @@
 #' # Get all Ocarina of Time 100% runs:
 #' runs <- get_runs(game = "j1l9qz1g", category = "q255jw2o")
 #' }
-get_runs <- function(game, category, max = 100, status = "verified", ...) {
+get_runs <- function(game, category, max = 100, status = "verified", offset = NULL, verbose = FALSE, ...) {
   res <- sr_get("runs",
     game = game, category = category, max = max,
-    status = status,
+    status = status, offset = offset,
     orderby = "submitted", direction = "desc", ...
   )
 
   next_url <- purrr::map_df(res$pagination$links, tibble::as_tibble)
 
-  if (!(identical(next_url, tibble::tibble()))) {
+  if (!("next" %in% next_url$rel)) {
+    next_url <- NA
+  } else if (!(identical(next_url, tibble::tibble()))) {
     next_url <- next_url$uri[next_url$rel == "next"]
   } else {
     next_url <- NA
@@ -35,8 +40,11 @@ get_runs <- function(game, category, max = 100, status = "verified", ...) {
   runs <- purrr::map_df(data, extract_run)
 
   # Pagination is hard and this doesn't work properly yet
-  # cat("\nnrow(runs) = ", nrow(runs))
-  # cat("\nmax = ", max)
+  if (verbose) {
+    cat("\nnrow(runs) = ", nrow(runs))
+    cat("\nmax = ", max)
+  }
+
   if (!is.na(next_url) & nrow(runs) < max & nrow(runs) != 0) {
     offset <- purrr::pluck(httr::parse_url(next_url), "query", "offset", .default = "")
     max <- max - as.numeric(offset)
